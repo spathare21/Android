3745622036c411ad8aca6fe7bc5320d8b1b69262
Dipak Pawar <dipak@vertisinfotech.com>
2016-01-25 17:04:44 +0530

    removed inheretance form freewheel.

--- src/test/java/testpackage/tests/freewheelsampleapp/BasicTests.java
+++ src/test/java/testpackage/tests/freewheelsampleapp/BasicTests.java
@@ -8,14 +8,6 @@ import java.io.IOException;
 /**
  * Created by Vertis on 14/01/16.
  */
-public class BasicTests extends SDK{
-
-        public BasicTests() throws IOException {
-            System.out.printf("In Basic Tests");
-            LoadPropertyValues config = new LoadPropertyValues();
-            propFileName = "./src/test/resources/config/freewheelSampleApp.properties";
-            //propFileName = "./src/test/resources/basicplaybacksampleapp.properties";
-            propertyReader =  config.loadProperty(propFileName);
-        }
+public class BasicTests{
 
 }
