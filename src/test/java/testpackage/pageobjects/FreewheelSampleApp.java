adbac40dab30f4ee03062fb974b59090f81e68ba
sachin.bukkawar <sachin.bukkawar@vertisinfotech.com>
2016-02-15 19:32:48 +0530

    test cases add for freewheel

--- src/test/java/testpackage/pageobjects/FreewheelSampleApp.java
+++ src/test/java/testpackage/pageobjects/FreewheelSampleApp.java
@@ -2,6 +2,7 @@ package testpackage.pageobjects;
 
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.Assert;
+import org.omg.PortableInterceptor.AdapterNameHelper;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -66,4 +67,61 @@ public class FreewheelSampleApp {
         imageButtons.get(index).click();
     }
 
-}
+    public void clickLearnMore(AndroidDriver driver) {
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//android.widget.TextView[@text='Learn More']")));
+        driver.findElement(By.xpath("//android.widget.TextView[@text='Learn More']")).click();
+
+    }
+        public void adPause (AndroidDriver driver)
+    {
+         driver.findElement(By.id("android:id/pause")).click();
+    }
+
+    public void adPlay (AndroidDriver driver)
+    {
+        driver.findElement(By.id("android:id/pause")).click();
+    }
+
+
+    public void getBackFromRecentApp (AndroidDriver driver) throws InterruptedException {
+
+
+        driver.sendKeyEvent(187);   //key 187 is used to go on recent app
+        System.out.println("key sent");
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//android.view.View[@index= '0']")).click();  // here clicking on system ui to get back the sample app
+        System.out.println("back to SDK");
+    }
+
+
+
+    public void powerKeyClick (AndroidDriver driver) throws InterruptedException {
+
+        driver.sendKeyEvent(26);            // key 26 is used to lock the screen
+        System.out.println("key sent");
+        System.out.println("screen lock");
+        Thread.sleep(5000);
+        driver.sendKeyEvent(82);            // key 82 is used to unlock the screen
+        System.out.println("key sent");
+        System.out.println("screen unlock");
+        Thread.sleep(2000);
+    }
+
+    public void videoPlay (AndroidDriver driver)
+    {
+        driver.findElement(By.xpath("//android.widget.ImageButton[@index ='0']")).click();
+    }
+
+    public void getXYSeekBarAndSeek(AndroidDriver driver, int widthOffSet1, int widthOffSet2) {
+        WebElement seekBarField = driver.findElement(By.xpath("//android.widget.SeekBar"));
+
+        int seekBarFieldWidth = seekBarField.getLocation().getX();
+        int seekBarFieldHeigth = seekBarField.getLocation().getY();
+        //System.out.println(" Dimensions bounds value is :-"+seekBarFieldHeigth);
+        //System.out.println(" Dimensions bounds value is :-"+seekBarFieldWidth);
+        System.out.println(" Seeking -------------------------  ");
+        driver.swipe(seekBarFieldWidth + widthOffSet1, seekBarFieldHeigth, seekBarFieldWidth + widthOffSet2, seekBarFieldHeigth, 3);
+    }
+    }
+
