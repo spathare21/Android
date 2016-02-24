0e2442eab068145db62d22468dda9cf906571201
bsondur <bsondur@ooyala.com>
2016-02-23 16:04:33 -0800

    QuickLookooyalaSampleApp1

---
+++ src/test/java/testpackage/pageobjects/ooyalaSkinSampleApp.java
@@ -0,0 +1,57 @@
+package testpackage.pageobjects;
+
+/**
+ * Created by bsondur on 2/23/16.
+ */
+
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.Assert;
+import org.omg.PortableInterceptor.AdapterNameHelper;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
+
+public class ooyalaSkinSampleApp {
+
+    public void waitForAppHomeScreen(AndroidDriver driver) {
+
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.
+                xpath("//android.widget.TextView[@text='Skin Playback']")));
+
+    }
+
+    public void assertCurrentActivityAgainst(AndroidDriver driver, String activityName) {
+
+        Assert.assertEquals(driver.currentActivity(), activityName);
+    }
+
+    public void clickBasedOnText(AndroidDriver driver, String clickText) {
+
+        WebElement clickTextField = driver.findElement(By.xpath("//android.widget.TextView[@text='" + clickText + "']"));
+        clickTextField.click();
+
+    }
+
+    public void waitForPresence(AndroidDriver driver, String typeOf, String waitString) {
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        if (typeOf == "className") {
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.className(waitString)));
+        }
+
+    }
+
+    public void waitForPresenceOfText(AndroidDriver driver,String waitString) {
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.name(waitString)));
+    }
+}
