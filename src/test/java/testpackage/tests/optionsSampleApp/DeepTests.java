7a8dcbebd7012e8d9aa93dc6d62b7691e1c9487c
rohan <rohan.rasane@vertisinfotech.com>
2016-04-29 13:06:41 +0530

    Started work on OptionSampleApp and able to lauch option sample app

---
+++ src/test/java/testpackage/tests/optionsSampleApp/DeepTests.java
@@ -0,0 +1,93 @@
+package testpackage.tests.optionsSampleApp;
+
+import io.appium.java_client.android.AndroidDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import testpackage.utils.*;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Created by Rohan R on 4/29/2016.
+ */
+public class DeepTests {
+
+    private static AndroidDriver driver;
+
+    @BeforeClass
+    public void beforeTest() throws Exception {
+        // closing all recent app from background.
+        CloserecentApps.closeApps();
+        System.out.println("BeforeTest \n");
+
+        System.out.println(System.getProperty("user.dir"));
+        // Get Property Values
+        LoadPropertyValues prop = new LoadPropertyValues();
+        Properties p = prop.loadProperty("optionsSampleApp.properties");
+
+        //System.out.println("Device id from properties file " + p.getProperty("deviceName"));
+        //System.out.println("PortraitMode from properties file " + p.getProperty("PortraitMode"));
+        //System.out.println("Path where APK is stored"+ p.getProperty("appDir"));
+        //System.out.println("APK name is "+ p.getProperty("app"));
+        //System.out.println("Platform under Test is "+ p.getProperty("platformName"));
+        //System.out.println("Mobile OS Version is "+ p.getProperty("OSVERSION"));
+        //System.out.println("Package Name of the App is "+ p.getProperty("appPackage"));
+        //System.out.println("Activity Name of the App is "+ p.getProperty("appActivity"));
+
+        SetUpAndroidDriver setUpdriver = new SetUpAndroidDriver();
+        driver = setUpdriver.setUpandReturnAndroidDriver(p.getProperty("udid"), p.getProperty("appDir"), p.getProperty("appValue"), p.getProperty("platformName"), p.getProperty("platformVersion"), p.getProperty("appPackage"), p.getProperty("appActivity"));
+        Thread.sleep(2000);
+    }
+
+    @BeforeMethod
+    //public void beforeTest() throws Exception{
+    public void beforeMethod() throws Exception {
+        System.out.println("beforeMethod \n");
+        //removeEventsLogFile.removeEventsFileLog(); create events file
+        PushLogFileToDevice logpush = new PushLogFileToDevice();
+        logpush.pushLogFile();
+        if (driver.currentActivity() != "com.ooyala.sample.lists.OptionsListActivity") {
+            driver.startActivity("com.ooyala.sample.OptionsSampleApp", "com.ooyala.sample.lists.OptionsListActivity");
+        }
+
+        // Get Property Values
+        LoadPropertyValues prop1 = new LoadPropertyValues();
+        Properties p1 = prop1.loadProperty();
+
+        System.out.println(" Screen Mode " + p1.getProperty("ScreenMode"));
+
+        //if(p1.getProperty("ScreenMode") != "P"){
+        //    System.out.println("Inside landscape Mode ");
+        //    driver.rotate(ScreenOrientation.LANDSCAPE);
+        //}
+
+        //driver.rotate(ScreenOrientation.LANDSCAPE);
+        //driver.rotate(ScreenOrientation.LANDSCAPE);
+
+    }
+
+    @AfterClass
+    public void afterTest() throws InterruptedException, IOException {
+        System.out.println("AfterTest \n");
+        driver.closeApp();
+        driver.quit();
+        LoadPropertyValues prop1 = new LoadPropertyValues();
+        Properties p1 = prop1.loadProperty();
+        String prop = p1.getProperty("appPackage");
+        Appuninstall.uninstall(prop);
+
+    }
+
+    @AfterMethod
+    //public void afterTest() throws InterruptedException, IOException {
+    public void afterMethod() throws InterruptedException, IOException {
+        // Waiting for all the events from sdk to come in .
+        System.out.println("AfterMethod \n");
+        //ScreenshotDevice.screenshot(driver);
+        RemoveEventsLogFile.removeEventsFileLog();
+        Thread.sleep(10000);
+    }
+}
