8a24f98ac0b669d9867ed4801ef553393f82f2db
Sachin Pathare <sachin.pathare@vertisinfotech.com>
2016-05-03 11:37:07 +0530

    started working on  exoplayer sample app

--- src/test/java/testpackage/pageobjects/exoPlayerSampleApp.java
+++ src/test/java/testpackage/pageobjects/exoPlayerSampleApp.java
@@ -4,6 +4,7 @@ import io.appium.java_client.android.AndroidDriver;
 import org.apache.xpath.operations.And;
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -14,6 +15,8 @@ import java.util.List;
  * Created by Sachin on 4/5/2016.
  */
 public class exoPlayerSampleApp {
+    Point replay, more, close_button, share_asset, discovery_button, cc_button, volume_button, enablecc_button;
+
     public void waitForAppHomeScreen(AndroidDriver driver) {
 
         WebDriverWait wait = new WebDriverWait(driver, 30);
@@ -48,37 +51,124 @@ public class exoPlayerSampleApp {
         buttons.get(index).click();
     }
 
-    public void waitForPresenceOfText(AndroidDriver driver,String waitString) {
+    public void waitForPresenceOfText(AndroidDriver driver, String waitString) {
         WebDriverWait wait = new WebDriverWait(driver, 30);
         wait.until(ExpectedConditions.presenceOfElementLocated(By.name(waitString)));
     }
 
 
-
-    public void verifyOverlay (AndroidDriver driver)
-    {
+    public void verifyOverlay(AndroidDriver driver) {
         WebElement element = driver.findElement(By.xpath("//android.view.View[@index = '0']"));
-        if (element.isDisplayed())
-        {
+        if (element.isDisplayed()) {
             System.out.println("overlay displayed");
         }
     }
 
-    public void skipAd (AndroidDriver driver) throws InterruptedException {
+    public void skipAd(AndroidDriver driver) throws InterruptedException {
         System.out.println("in skip ad");
         Thread.sleep(7000);
-       WebElement element =  (new WebDriverWait(driver, 30)).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.Button[@index='0']")));
-               element.click();
-    // driver.tap(1,585,1524,2);
+        WebElement element = (new WebDriverWait(driver, 30)).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.Button[@index='0']")));
+        element.click();
+        // driver.tap(1,585,1524,2);
     }
 
-    public void adPause (AndroidDriver driver)
-    {
+    public void adPause(AndroidDriver driver) {
         driver.findElement(By.className("android.view.View")).click();
     }
 
     public void clickBasedOnTextScrollTo(AndroidDriver driver, String clickText) {
 
         driver.scrollTo(clickText).click();
+    }
+
+
+    public void replayVideo(AndroidDriver driver) throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        String path = "//android.widget.TextView[@text='c']";
+
+        WebElement ele = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(path)));
+        replay = ele.getLocation();
+        System.out.println("replay.x value is " + replay.getX());
+        System.out.println("replay.y value is " + replay.getY());
+
+        Thread.sleep(2000);
+
+        // more button location
+        WebElement button = driver.findElement(By.xpath("//android.widget.TextView[@text='f']"));
+        more = button.getLocation();
+        System.out.printf("more button's X  cordinates" + more.getX());
+        System.out.printf("more button's y  cordinates" + more.getY());
+        driver.tap(1, more.getX(), more.getY(), 2);
+
+        Thread.sleep(2000);
+        // click on close button
+        WebElement close = driver.findElement(By.xpath("//android.widget.TextView[@text='e']"));
+        close_button = close.getLocation();
+        System.out.printf("close button's X  cordinates" + close_button.getX());
+        System.out.printf("close button's y  cordinates" + close_button.getY());
+//       // driver.tap(1,close_button.getX(),close_button.getY(),2);
+
+        Thread.sleep(2000);
+        // shareAsset button location
+        WebElement share = driver.findElement(By.xpath("//android.widget.TextView[@text='o']"));
+        share_asset = share.getLocation();
+        System.out.printf("share button's X  cordinates" + share_asset.getX());
+        System.out.printf("share button's y  cordinates" + share_asset.getY());
+        driver.tap(1, share_asset.getX(), share_asset.getY(), 2);
+        Thread.sleep(5000);
+        System.out.println("clicked on shared button");
+
+        Thread.sleep(2000);
+
+        System.out.println("clicking on screen");
+
+        driver.tap(1, 0, 75, 2);
+        System.out.println("tapped");
+
+        Thread.sleep(2000);
+        // Discovery button lcoation
+        WebElement discovery = driver.findElementByXPath("//android.widget.TextView[@text='l']");
+        discovery_button = discovery.getLocation();
+        System.out.printf("discovery button's X  cordinates" + discovery_button.getX());
+        System.out.printf("discovery button's y  cordinates" + discovery_button.getY());
+
+        Thread.sleep(2000);
+
+        //CC button location
+        WebElement CC = driver.findElementByXPath("//android.widget.TextView[@text='k']");
+        cc_button = CC.getLocation();
+        System.out.printf("CC button's X  cordinates" + cc_button.getX());
+        System.out.printf(" CC bbutton's y  cordinates" + cc_button.getY());
+        driver.tap(1, cc_button.getX(), cc_button.getY(), 2);
+        Thread.sleep(2000);
+
+        //emable CC locations
+
+        WebElement enablecc = driver.findElementByXPath("//android.widget.Switch[@index='4']");
+        enablecc_button = enablecc.getLocation();
+        System.out.printf("enablecc button's X  cordinates" + enablecc_button.getX());
+        System.out.printf(" enablecc button's y  cordinates" + enablecc_button.getY());
+
+
+        Thread.sleep(2000);
+        driver.tap(1, close_button.getX(), close_button.getY(), 2);
+        System.out.println("CC option closed");
+
+        Thread.sleep(2000);
+        driver.tap(1, close_button.getX(), close_button.getY(), 2);
+        System.out.println("more option closed");
+
+        Thread.sleep(2000);
+        // volume button location
+        WebElement volume = driver.findElementByXPath("//android.widget.TextView[@text='b']");
+        volume_button = volume.getLocation();
+        System.out.printf("volume button's X  cordinates" + volume_button.getX());
+        System.out.printf(" volume button's y  cordinates" + volume_button.getY());
+
+        Thread.sleep(1000);
+
+        System.out.println("printed all the locations");
+
+        ele.click();
     }
 }
---
+++ src/test/java/testpackage/tests/exoPlayerSampleApp/DeepTestsFW.java
@@ -0,0 +1,167 @@
+package testpackage.tests.exoPlayerSampleApp;
+
+import io.appium.java_client.android.AndroidDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import testpackage.pageobjects.exoPlayerSampleApp;
+import testpackage.utils.*;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Created by sumit on 03/05/16.
+ */
+public class DeepTestsFW {
+    private static AndroidDriver driver;
+
+    @BeforeClass
+
+    public void beforeTest() throws Exception {
+
+        // closing all recent app from background.
+        CloserecentApps.closeApps();
+        System.out.println("BeforeTest \n");
+
+        System.out.println(System.getProperty("user.dir"));
+        // Get Property Values
+        LoadPropertyValues prop = new LoadPropertyValues();
+        Properties p=prop.loadProperty("exoPlayerSampleApp.properties");
+
+        System.out.println("Device id from properties file " + p.getProperty("deviceName"));
+        System.out.println("PortraitMode from properties file " + p.getProperty("PortraitMode"));
+        System.out.println("Path where APK is stored"+ p.getProperty("appDir"));
+        System.out.println("APK name is "+ p.getProperty("app"));
+        System.out.println("Platform under Test is "+ p.getProperty("platformName"));
+        System.out.println("Mobile OS Version is "+ p.getProperty("OSVERSION"));
+        System.out.println("Package Name of the App is "+ p.getProperty("appPackage"));
+        System.out.println("Activity Name of the App is "+ p.getProperty("appActivity"));
+
+        SetUpAndroidDriver setUpdriver = new SetUpAndroidDriver();
+        driver = setUpdriver.setUpandReturnAndroidDriver(p.getProperty("udid"), p.getProperty("appDir"), p.getProperty("appValue"), p.getProperty("platformName"), p.getProperty("platformVersion"), p.getProperty("appPackage"), p.getProperty("appActivity"));
+        Thread.sleep(2000);
+    }
+
+    @BeforeMethod
+    public void beforeMethod() throws Exception {
+        System.out.println("beforeMethod \n");
+        //removeEventsLogFile.removeEventsFileLog(); create events file
+        PushLogFileToDevice logpush=new PushLogFileToDevice();
+        logpush.pushLogFile();
+        if(driver.currentActivity()!= "com.ooyala.sample.complete.MainExoPlayerActivity") {
+            driver.startActivity("com.ooyala.sample.ExoPlayerSampleApp","com.ooyala.sample.complete.MainExoPlayerActivity");
+        }
+
+        // Get Property Values
+        LoadPropertyValues prop1 = new LoadPropertyValues();
+        Properties p1=prop1.loadProperty();
+
+        System.out.println(" Screen Mode "+ p1.getProperty("ScreenMode"));
+
+        //if(p1.getProperty("ScreenMode") != "P"){
+        //    System.out.println("Inside landscape Mode ");
+        //    driver.rotate(ScreenOrientation.LANDSCAPE);
+        //}
+
+        //driver.rotate(ScreenOrientation.LANDSCAPE);
+        //driver.rotate(ScreenOrientation.LANDSCAPE);
+
+    }
+    @AfterClass
+    public void afterTest() throws InterruptedException, IOException {
+        System.out.println("AfterTest \n");
+        driver.closeApp();
+        driver.quit();
+        LoadPropertyValues prop1 = new LoadPropertyValues();
+        Properties p1 = prop1.loadProperty();
+        String prop = p1.getProperty("appPackage");
+        Appuninstall.uninstall(prop);
+
+    }
+
+    @AfterMethod
+    public void afterMethod() throws InterruptedException, IOException {
+        // Waiting for all the events from sdk to come in .
+        System.out.println("AfterMethod \n");
+        //ScreenshotDevice.screenshot(driver);
+        RemoveEventsLogFile.removeEventsFileLog();
+        Thread.sleep(10000);
+
+    }
+
+
+    @org.testng.annotations.Test
+    public void FreeWheelIntegrationPreRoll() throws Exception{
+
+        try {
+            // Creating an Object of FreeWheelSampleApp class
+            exoPlayerSampleApp po = new exoPlayerSampleApp();
+            // wait till home screen of basicPlayBackApp is opened
+            po.waitForAppHomeScreen(driver);
+
+
+            // Assert if current activity is indeed equal to the activity name of app home screen
+            po.assertCurrentActivityAgainst(driver, "com.ooyala.sample.complete.MainExoPlayerActivity");
+            // Wrire to console activity name of home screen app
+            System.out.println("Ooyala Skin Sample App Launched successfully. Activity :- " + driver.currentActivity() + "\n");
+
+            //Pause the running of test for a brief time .
+            Thread.sleep(3000);
+
+            po.clickBasedOnText(driver, "Freewheel Integration");
+            Thread.sleep(2000);
+
+            // Assert if current activity is Freewheel list activity
+            po.assertCurrentActivityAgainst(driver, "com.ooyala.sample.lists.FreewheelListActivity");
+
+            // Select one of the video HLS,MP4 etc .
+            po.clickBasedOnText(driver, "Freewheel Preroll");
+            Thread.sleep(2000);
+
+            System.out.println("clicked on based text");
+
+
+            //verify if player was loaded
+            po.waitForPresence(driver, "className", "android.view.View");
+            // Assert if current activity is indeed equal to the activity name of the video player
+            po.assertCurrentActivityAgainst(driver, "com.ooyala.sample.players.PreconfiguredFreewheelPlayerActivity");
+            // Print to console output current player activity
+            System.out.println("Player Video was loaded successfully . Activity  :- " + driver.currentActivity() + "\n");
+
+            po.waitForPresenceOfText(driver,"h");
+
+            //Clicking on Play button in Ooyala Skin
+            po.clickBasedOnText(driver,"h");
+
+
+
+            //Play Started Verification
+            EventVerification ev = new EventVerification();
+            ev.verifyEvent("adStarted", " Ad Started to Play ", 30000);
+
+            Thread.sleep(5000);
+
+            ev.verifyEvent("adCompleted", " Ad Completed to Play ", 30000);
+
+            //Wait for video to start and verify the playStarted event .
+            ev.verifyEvent("playStarted", " Video Started Play ", 30000);
+
+            //Wait for video to finish and verify the playCompleted event .
+            ev.verifyEvent("playCompleted", " Video Completed Play ", 70000);
+
+            po.replayVideo(driver);
+
+            Thread.sleep(30000);
+
+        }
+        catch(Exception e)
+        {
+            System.out.println(" Exception "+e);
+            e.printStackTrace();
+            ScreenshotDevice.screenshot(driver);
+        }
+
+    }
+}
