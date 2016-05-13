8bbef8030d8f6112ea3c13d18a2e054a11b6fbef
Sachin Pathare <shiam.gupta@vertisinfotech.com>
2016-05-13 14:31:42 +0530

    comment

--- src/test/java/testpackage/tests/ooyalaSkinSampleApp/DeepTestIMA.java
+++ src/test/java/testpackage/tests/ooyalaSkinSampleApp/DeepTestIMA.java
@@ -92,7 +92,7 @@ public class DeepTestIMA {
 
     }
 
-
+/*
     @org.testng.annotations.Test
     public void GoogleIMAIntegrationIMAAdRulesPreroll() throws Exception {
         int[] locPlayButon;
@@ -1492,7 +1492,7 @@ public class DeepTestIMA {
             e.printStackTrace();
             ScreenshotDevice.screenshot(driver);
         }
-    }
+    }*/
 
     @org.testng.annotations.Test
     public void GoogleIMAIntegrationIMASkippable() throws Exception{
@@ -1679,7 +1679,7 @@ public class DeepTestIMA {
             ScreenshotDevice.screenshot(driver);
         }
     }
-
+/*
     @org.testng.annotations.Test
     public void GoogleIMAIntegrationIMAPreMidPostSkippable() throws Exception {
         int[] locPlayButon;
@@ -1889,5 +1889,5 @@ public class DeepTestIMA {
             e.printStackTrace();
             ScreenshotDevice.screenshot(driver);
         }
-    }
+    }*/
 }
