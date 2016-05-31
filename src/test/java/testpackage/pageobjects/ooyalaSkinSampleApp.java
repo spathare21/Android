8af0bb72b8b24278f5a024a6659216b70cace556
bsondur <bsondur@ooyala.com>
2016-05-31 11:45:35 -0700

    resolvingConflictexo_vertis2

--- src/test/java/testpackage/pageobjects/ooyalaSkinSampleApp.java
+++ src/test/java/testpackage/pageobjects/ooyalaSkinSampleApp.java
@@ -209,8 +209,7 @@ public class ooyalaSkinSampleApp {
         System.out.println("replay.x value is " + replay.getX());
         System.out.println("replay.x value is " + replay.getY());
 
-
-               driver.tap(1, replay.getX(), replay.getY(), 2);
+                driver.tap(1, replay.getX(), replay.getY(), 2);
                 System.out.println("clicked pause");
 
 
