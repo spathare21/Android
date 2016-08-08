7ebf1a1f72aa9aff1b485d56e9333e89bee97096
shivam gupta <shiam.gupta@vertisinfotech.com>
2016-08-08 15:07:33 +0530

    Pulse Sample App

---
+++ src/test/java/testpackage/tests/PulseSampleApp/BasicTests.java
@@ -0,0 +1,90 @@
+package testpackage.tests.PulseSampleApp;
+
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import testpackage.utils.*;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Created by Shivam on 05/08/16.
+ */
+public class BasicTests extends EventLogTest {
+    @BeforeClass
+    public void beforeTest() throws Exception {
+        // closing all recent app from background.
+        CloserecentApps.closeApps();
+        System.out.println("BeforeTest \n");
+
+        System.out.println(System.getProperty("user.dir"));
+        // Get Property Values
+        LoadPropertyValues prop = new LoadPropertyValues();
+        Properties p = prop.loadProperty("pulsesampleapp.properties");
+
+        //System.out.println("Device id from properties file " + p.getProperty("deviceName"));
+        //System.out.println("PortraitMode from properties file " + p.getProperty("PortraitMode"));
+        //System.out.println("Path where APK is stored"+ p.getProperty("appDir"));
+        //System.out.println("APK name is "+ p.getProperty("app"));
+        //System.out.println("Platform under Test is "+ p.getProperty("platformName"));
+        //System.out.println("Mobile OS Version is "+ p.getProperty("OSVERSION"));
+        //System.out.println("Package Name of the App is "+ p.getProperty("appPackage"));
+        //System.out.println("Activity Name of the App is "+ p.getProperty("appActivity"));
+
+        SetUpAndroidDriver setUpdriver = new SetUpAndroidDriver();
+        driver = setUpdriver.setUpandReturnAndroidDriver(p.getProperty("udid"), p.getProperty("appDir"), p.getProperty("appValue"), p.getProperty("platformName"), p.getProperty("platformVersion"), p.getProperty("appPackage"), p.getProperty("appActivity"));
+        Thread.sleep(2000);
+    }
+
+    @BeforeMethod
+    public void beforeMethod() throws Exception {
+        System.out.println("beforeMethod \n");
+        //removeEventsLogFile.removeEventsFileLog(); create events file
+        PushLogFileToDevice logpush = new PushLogFileToDevice();
+        logpush.pushLogFile();
+        if (driver.currentActivity() != "com.ooyala.sample.lists.PulseListActivity") {
+            driver.startActivity("com.ooyala.sample.PulseSampleApp", "com.ooyala.sample.lists.PulseListActivity");
+        }
+
+        // Get Property Values
+        LoadPropertyValues prop1 = new LoadPropertyValues();
+        Properties p1 = prop1.loadProperty();
+
+        System.out.println(" Screen Mode " + p1.getProperty("ScreenMode"));
+
+        //if(p1.getProperty("ScreenMode") != "P"){
+        //    System.out.println("Inside landscape Mode ");
+        //    driver.rotate(ScreenOrientation.LANDSCAPE);
+        //}
+
+        //driver.rotate(ScreenOrientation.LANDSCAPE);
+        //driver.rotate(ScreenOrientation.LANDSCAPE);
+
+    }
+    @AfterClass
+    public void afterTest() throws InterruptedException, IOException {
+        System.out.println("AfterTest \n");
+        driver.closeApp();
+        driver.quit();
+        LoadPropertyValues prop1 = new LoadPropertyValues();
+        Properties p1 = prop1.loadProperty();
+        String prop = p1.getProperty("appPackage");
+        Appuninstall.uninstall(prop);
+
+    }
+
+    @AfterMethod
+    //public void afterTest() throws InterruptedException, IOException {
+    public void afterMethod(ITestResult result) throws Exception {
+        // Waiting for all the events from sdk to come in .
+        System.out.println("AfterMethod \n");
+        //ScreenshotDevice.screenshot(driver);
+        RemoveEventsLogFile.removeEventsFileLog();
+        Thread.sleep(10000);
+
+    }
+
+}
