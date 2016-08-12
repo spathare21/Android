48a50e4db0c3ea665338f7d043806d2e42347cb6
Sachin Pathare <sachin.pathare@vertisinfotech.com>
2016-08-12 11:48:34 +0530

    modified Listener code

--- src/test/java/testpackage/utils/Listeners.java
+++ src/test/java/testpackage/utils/Listeners.java
@@ -5,11 +5,14 @@ import org.testng.*;
 import org.testng.xml.XmlSuite;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 
 public class Listeners implements  IReporter {
+
+    Map<String,String> data = new HashMap<String, String>();
 
     @Override
     public void generateReport(List<XmlSuite> xmlSuite, List<ISuite> iSuites, String s){
@@ -20,6 +23,7 @@ public class Listeners implements  IReporter {
             for (ISuiteResult r : result.values()) {
                 ITestContext context = r.getTestContext();
                 System.out.println("Date : " + today.toString());
+                data.put("",Integer.toString(context.getPassedTests().size()).toString());
                 System.out.println("Passed Tests : " + context.getPassedTests().size());
                 System.out.println("Failed Tests : "+ context.getFailedTests().size());
                 System.out.println("Skipped Tests : "+ context.getSkippedTests().size());
@@ -29,6 +33,13 @@ public class Listeners implements  IReporter {
                 for(int i=0 ; i< psize ; i++){
                     System.out.println("Package Name : " + context.getSuite().getXmlSuite().getTests().get(i).getClasses().get(i).getName());
                 }
+            try {
+                Adblogcat.deviceinfo();
+                 Adblogcat.androidVersion();
+                }catch (Exception e){
+
+            }
+
 
                 //System.out.println("Package Name : " + context.getSuite().getXmlSuite().getTests().get(0).getClasses().get(0).getName());
             }
@@ -40,4 +51,16 @@ public class Listeners implements  IReporter {
 */
 
     }
+
+    public void insertData(int pass,int fail,int skip,int total,String suitename,String packagename) throws Exception
+    {
+        Date today = new Date();
+        today.toString();
+        Adblogcat.deviceinfo();
+        Adblogcat.androidVersion();
+        //Adblogcat.sdkVersion();
+
+    }
+
+
 }
